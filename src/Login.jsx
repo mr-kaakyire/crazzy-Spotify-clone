8c9bf27f1e92c9fb7f7a279f3d3fd291d8cbf611
Mr-Boahen <emmanuelnyantakyiboahen@gmail.com>
2023-01-08 02:48:03 +0000

    button styling 2nd

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,7 +6,10 @@ function Login() {
   return (
     <div className='grid place-items-center h-screen bg-black'>
         <img className='w-[100%]' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="" />
-        <a href={loginUrl} className='p-[10px] w-64 mx-auto bg-[#1db954] active:bg-[#17873f] rounded-3xl font-semibold  text-black decoration-0'>LOGIN WITH SPOTIFY</a>
+        <div  className='p-[10px] w-[480px] px-12 flex items-center justify-center bg-[#1db954] active:bg-[#17873f]  rounded-3xl font-[600]  text-[#373737] decoration-0'>
+          <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+        </div>
+        
     </div>
   )
 }
