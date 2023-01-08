d04896a9a255232954826a1cfae98d99d697c1c4
Mr-Boahen <emmanuelnyantakyiboahen@gmail.com>
2023-01-08 23:04:07 +0000

    media query 2nd

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
     <div className='grid place-items-center h-screen bg-black'>
         <img className='w-[100%]' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="" />
         
-          <a className='p-[10px]  px-12 flex items-center justify-center bg-[#1db954] active:bg-[#17873f]  rounded-3xl font-[600]  text-[#373737] decoration-0' href={loginUrl}>LOGIN WITH SPOTIFY</a>
+          <a className='p-[10px]  px-12 flex items-center justify-center bg-[#1db954] active:bg-[#17873f] active:scale-105 rounded-3xl font-[600]  text-[#373737] decoration-0' href={loginUrl}>LOGIN WITH SPOTIFY</a>
         
         
     </div>
