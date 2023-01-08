c424ea7cec130186207010d358f0a85b92ad07c0
Mr-Boahen <emmanuelnyantakyiboahen@gmail.com>
2023-01-08 02:29:00 +0000

    login button styling

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
   return (
     <div className='grid place-items-center h-screen bg-black'>
         <img className='w-[100%]' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="" />
-        <a href={loginUrl} className='p-3 bg-[#1db954] active:bg-[#17873f] rounded-3xl font-semibold  text-white decoration-0'>LOGIN WITH SPOTIFY</a>
+        <a href={loginUrl} className='p-[10px] w-64 mx-auto bg-[#1db954] active:bg-[#17873f] rounded-3xl font-semibold  text-black decoration-0'>LOGIN WITH SPOTIFY</a>
     </div>
   )
 }
